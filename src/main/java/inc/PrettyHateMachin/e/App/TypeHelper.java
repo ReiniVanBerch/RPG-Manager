ab513226981cfeac2bf46ebd4de8c9336a5d1567
ReiniVanBerch <69420774+ReiniVanBerch@users.noreply.github.com>
2024-12-06 20:28:38 +0100

    Reconfiguring the project and some slight work on TypeHelper.java

--- src/main/java/inc/PrettyHateMachin/e/App/TypeHelper.java
+++ src/main/java/inc/PrettyHateMachin/e/App/TypeHelper.java
@@ -1,12 +1,22 @@
 package inc.PrettyHateMachin.e.App;
 
-import org.json.*;
+//import org.json.*;
 
 /**
  * @author Vincent Berchtold
- * @version 0.1.01
+ * @version 0.1.2
+ *
+ *
+ * The type is represented in hexadecimal, but is just 1 Byte.
+ * We split the byte into two half-bytes where the ...
+ * ... first represents the "type", typeInt, so whether its an type o integer, double, boolean, whatever
+ * ... second represetns the "quality", qualityInt, so whether its a sole value, a range, an array etc.
+ *      -   the quality is translatable between types, so *F will always be an array,
+ *          some have only theoretical types, it lets you generate boolean ranges, just not really ~worthy~
+ *
  *
  * Types for the kind of information to be stored
+ * as of 06.12.2024, day of the comeback
  *
  * 0# / Boolean
  * 00 - Boolean
@@ -42,17 +52,23 @@ import org.json.*;
 public class TypeHelper {
     //Handle the different Types - just put in the "value" from character2property
 
+    public static Object typeResolver(int typeNumber, String value){
+        int typeInt = typeNumber / 16;
+        int qualityInt = typeNumber % 16;
+
+
+    }
+
     public static Object jsonResolver(String json){
 
 
-        return null;
-    }
 
-    public static Object jsonResolver(JSONObject json){
-        json.getJSONObject("properties");
 
         return null;
     }
+
+
+
 
 
 
