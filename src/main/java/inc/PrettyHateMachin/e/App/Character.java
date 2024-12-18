7ac242d74d3e3c19ac23545c59c312c965667a22
Florian Koller <koller.florian@gmail.com>
2024-12-18 19:32:57 +0100

    Update

---
+++ src/main/java/inc/PrettyHateMachin/e/App/Character.java
@@ -0,0 +1,25 @@
+package inc.PrettyHateMachin.e.App;
+
+public class Character {
+    private String Name;
+
+    /*List <> sachen
+    Claudius von Vengaberg  Cool RPG
+    Trevor Michael
+    RPG for Bros
+    -> Ideen für Sachen HP (int) XP ranged Value Name fixed String Background Story
+    Character Bild
+    Sachen:
+    Morbit
+    Character + Closebutton
+    Flo
+    Startseite JavaFX
+    Hera
+    Characterpage - JavaFX
+    Ayden
+
+
+
+    */
+
+}
