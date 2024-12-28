14be93ce026b39b57e437e533cd275bd710e596c
Florian Koller <koller.florian@gmail.com>
2024-12-29 00:54:55 +0100

    adding javafx

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("org.openjfx.javafxplugin") version "0.0.8"
 }
 
 group = "inc.PrettyHateMachin.e"
@@ -13,6 +14,9 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
+
+
+
 
 tasks.test {
     useJUnitPlatform()
