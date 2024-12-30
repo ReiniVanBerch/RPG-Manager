a7e7e52ae5bf03502ed1c2dd0c1c1da4b5be96b0
Florian Koller <koller.florian@gmail.com>
2024-12-30 10:15:51 +0100

    Example added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("org.openjfx.javafxplugin") version "0.0.8"
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "inc.PrettyHateMachin.e"
@@ -15,7 +15,9 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-
+javafx {
+    modules("javafx.controls", "javafx.fxml")
+}
 
 
 tasks.test {
