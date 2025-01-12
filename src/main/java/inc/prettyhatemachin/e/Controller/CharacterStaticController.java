9a21c4fa8628a49c9af77de0c76d65ea1ca6da8d
Ayden Kierein <ayden.kierein@stud.fh-campuswien.ac.at>
2025-01-12 18:24:29 +0100

    new structure static character static screen

--- src/main/java/inc/prettyhatemachin/e/Controller/CharacterStaticController.java
+++ src/main/java/inc/prettyhatemachin/e/Controller/CharacterStaticController.java
@@ -2,17 +2,72 @@
 package inc.prettyhatemachin.e.Controller;
 
 import inc.prettyhatemachin.e.App.Character;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 import java.util.ArrayList;
 
 public class CharacterStaticController {
 
-    private Character character;
+    private final Character character;
+
+    @FXML
+    private final StringProperty characterName;
+
+    @FXML
+    private TextField characterNameField;
+
+    @FXML
+    private final IntegerProperty characterHealth;
+
+    @FXML
+    private final IntegerProperty characterConstitution;
+
+    @FXML
+    private final IntegerProperty characterStrength;
+
+    @FXML
+    private final ListView<String> items;
+
+
+
 
     public CharacterStaticController(Character character){
         this.character = character;
+
+        characterName = character.nameProperty();
+        characterHealth = character.healthProperty();
+        characterConstitution = character.constitutionProperty();
+        characterStrength = character.strengthProperty();
+
+        ObservableList<String> observable = FXCollections.observableArrayList(character.getItems());
+        this.items = new ListView<>(observable);
+    }
+
+    @FXML
+    public void initialise() {
+
+    }
+
+    //TODO: gets called in initialise()
+    public void setCharacterName() {
+        characterNameField.setText(characterName.getName());
+    }
+
+    @FXML
+    //TODO: different method for each different characteristic so i know which one to increase/decrease?
+    private void increaseOne () {
+
+    }
+
+    @FXML
+    private void decreaseOne() {
+
     }
 
 }
